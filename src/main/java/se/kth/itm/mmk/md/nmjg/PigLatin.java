1ed4503e6399e99bf5365b048fbc1fe9c9bfd236
ejoagus <joagusta@kth.se>
2017-02-21 13:46:36 +0100

    Initial project setup.

---
+++ src/main/java/se/kth/itm/mmk/md/nmjg/PigLatin.java
@@ -0,0 +1,14 @@
+package se.kth.itm.mmk.md.nmjg;
+
+public class PigLatin {
+
+	public static void main(String[] args) {
+		PigLatin latin = new PigLatin();
+		System.out.println(latin.hello());
+	}
+	
+	public String hello(){
+		return "Hello World";
+	}
+
+}
